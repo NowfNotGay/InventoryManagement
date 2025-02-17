5ce013e24ba902dbf5a0241933e73d2930ace9b9
gau2209 <anhtuanvo2209@gmail.com>
2025-02-18 02:40:55 +0700

    Gau - TransactionType

---
+++ Microservice/Core/MasterData/TransactionType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.MasterData
+{
+    public class TransactionType : BaseClass.BaseClass
+    {
+        public string TransactionTypeCode { get; set; }
+        public string TransactionTypeName { get; set; }
+        public int DocumentTypeID { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Web_Service/API_Application/Controllers/MasterData/TransactionTypeController.cs
@@ -0,0 +1,76 @@
+﻿using Base.BaseService;
+using Base.MasterData;
+using Core.MasterData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Application.Controllers.MasterData
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionTypeController : ControllerBase
+    {
+        private readonly ITransactionTypeProvider _transactionTypeProvider;
+        private readonly ICRUD_Service<TransactionType, int> _ICRUD_Service;
+
+        public TransactionTypeController(ITransactionTypeProvider transactionTypeProvider, ICRUD_Service<TransactionType, int> iCRUD_Service)
+        {
+            _transactionTypeProvider = transactionTypeProvider;
+            _ICRUD_Service = iCRUD_Service;
+        }
+
+        [HttpGet]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetAll()
+        {
+            var rs = await _ICRUD_Service.GetAll();
+            return rs == null ? BadRequest() : Ok(rs);
+
+        }
+
+        [HttpGet("ID")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetByID([FromQuery] int ID)
+        {
+            var rs = await _ICRUD_Service.Get(ID);
+            return rs == null ? BadRequest() : Ok(rs);
+
+        }
+
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+
+        public async Task<IActionResult> Save([FromBody] TransactionType TransactionType)
+        {
+            var rs = await _ICRUD_Service.Create(TransactionType);
+            return rs == null ? BadRequest() : Ok(rs);
+        }
+
+        [HttpPut]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+
+        public async Task<IActionResult> Update([FromBody] TransactionType TransactionType)
+        {
+            var rs = await _ICRUD_Service.Update(TransactionType);
+            return rs == null ? BadRequest() : Ok(rs);
+
+        }
+        [HttpDelete]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var rs = await _ICRUD_Service.Delete(id);
+            return rs == null ? BadRequest() : Ok(rs);
+
+        }
+
+        
+    }
+}
