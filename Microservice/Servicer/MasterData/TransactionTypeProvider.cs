c412940834e23adfdb45203ad7b665b337d48ae2
gau2209 <anhtuanvo2209@gmail.com>
2025-02-23 16:43:25 +0700

    Gau - TransactionType

--- Microservice/Servicer/MasterData/TransactionTypeProvider.cs
+++ Microservice/Servicer/MasterData/TransactionTypeProvider.cs
@@ -15,10 +15,13 @@ using Helper.Method;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 using System.Transactions;
 using Microsoft.EntityFrameworkCore;
+using Core.BaseClass;
+using Google.Protobuf.WellKnownTypes;
+using System.Data.Common;
 
 namespace Servicer.MasterData
 {
-    public class TransactionTypeProvider : ICRUD_Service<TransactionType, int>, ITransactionTypeProvider
+    public class TransactionTypeProvider : ICRUD_Service_V2<TransactionType, int>, ITransactionTypeProvider
     {
         private readonly DB_MasterData_Context _Context;
         private readonly IConfiguration _configuration;
@@ -29,160 +32,320 @@ namespace Servicer.MasterData
         {
             _Context = context;
             _configuration = configuration;
+
         }
 
-        public async Task<TransactionType> Get(int id)
+        public async Task<ResultService<IEnumerable<TransactionType>>> GetAll()
         {
+            var response = new ResultService<IEnumerable<TransactionType>>();
+            string connectionString = General.DecryptString(_configuration.GetConnectionString(_moduleDapper));
             try
             {
-                string connectionString = General.DecryptString(_configuration.GetConnectionString(_moduleDapper));
-
-                using (var dbconnection = new SqlConnection(connectionString))
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
                 {
-                    await dbconnection.OpenAsync();
+                    await sqlConnection.OpenAsync();
+                    var result = await sqlConnection.QueryAsync<TransactionType>("TransactionType_GetAll",
+                        commandType: CommandType.StoredProcedure,
+                           commandTimeout: TimeoutInSeconds);
+
+                    response.Code = "0";// Success
+                    response.Message = "Success";
+                    response.Data = result;
+                    return response;
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                response.Code = "1";
+                response.Message = $"{sqlEx.GetType()} - {sqlEx.Message}";
+                return response;
+            }
+            catch (ArgumentException ex)
+            {
+                response.Code = "2";
+                response.Message = $"An error occurred while trying to connect to your database Server, pls check your Configuration .Details: {ex.GetType()} - {ex.Message}";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Code = "999";
+                response.Message = $"An error occurred while executing store Procedure. Details: {ex.GetType()} - {ex.Message}";
+                return response;
+            }
+        }
+
+        public async Task<ResultService<TransactionType>> Get(int id)
+        {
+            var response = new ResultService<TransactionType>();
+            string connectionString = General.DecryptString(_configuration.GetConnectionString(_moduleDapper));
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    await sqlConnection.OpenAsync();
                     var param = new DynamicParameters();
                     param.Add("@ID", id);
-                    return await dbconnection.QueryFirstOrDefaultAsync<TransactionType>("TransactionType_GetByID",
+                    var result = await sqlConnection.QuerySingleOrDefaultAsync<TransactionType>("TransactionType_GetByID",
                         param,
-                         commandType: CommandType.StoredProcedure,
-                            commandTimeout: TimeoutInSeconds);
+                        commandType: CommandType.StoredProcedure,
+                           commandTimeout: TimeoutInSeconds);
+                    if(result == null)
+                    {
+                        return new ResultService<TransactionType>()
+                        {
+                            Code = "-1",
+                            Message = "Entity not found",
+                            Data = null
+                        };
+                    }
+                    response.Code = "0";// Success
+                    response.Message = "Success";
+                    response.Data = result;
+                    return response;
                 }
             }
-            catch (SqlException ex)
+            catch (SqlException sqlEx)
             {
-                return null;
+                response.Code = "1";
+                response.Message = $"{sqlEx.GetType()} - {sqlEx.Message}";
+                return response;
             }
-
-
+            catch (ArgumentException ex)
+            {
+                response.Code = "2";
+                response.Message = $"An error occurred while trying to connect to your database Server, pls check your Configuration .Details: {ex.GetType()} - {ex.Message}";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Code = "999";
+                response.Message = $"An error occurred while executing store Procedure. Details: {ex.GetType()} - {ex.Message}";
+                return response;
+            }
         }
 
-        public async Task<IEnumerable<TransactionType>> GetAll()
+        public async Task<ResultService<TransactionType>> Create(TransactionType entity)
         {
-            try
+            var response = new ResultService<TransactionType>();
+            if (entity == null)
             {
-                string connectionString = General.DecryptString(_configuration.GetConnectionString(_moduleDapper));
-
-                using (var dbconnection = new SqlConnection(connectionString))
+                return new ResultService<TransactionType>()
                 {
-                    await dbconnection.OpenAsync();
-                    var param = new DynamicParameters();
-                    return await dbconnection.QueryAsync<TransactionType>("TransactionType_GetAll",
-                         commandType: CommandType.StoredProcedure,
-                            commandTimeout: TimeoutInSeconds);
-                }
+                    Code = "1",
+                    Message = "Entity is not valid"
+                };
             }
-            catch (SqlException ex)
-            {
-                return null;
-            }
-
-
-        }
-
-        public async Task<TransactionType> Create(TransactionType entity)
-        {
 
             using (var connection = _Context.Database.BeginTransaction())
             {
                 try
                 {
-                    entity.RowPointer = Guid.NewGuid();
+                    entity.RowPointer = Guid.Empty;
+                    entity.ID = 0;
                     await _Context.TransactionTypes.AddAsync(entity);
                     await _Context.SaveChangesAsync();
                     await connection.CommitAsync();
-                    return entity;
+
+                    response.Code = "0";// Success
+                    response.Message = "Create new Instance Successfully";
+                    response.Data = entity;
+                    return response;
+                }
+                catch (SqlException sqlex)
+                {
+                    await connection.RollbackAsync();
+                    //lỗi xảy ra khi có sự xung đột giữa các bản ghi trong cơ sở dữ liệu khi cố gắng cập nhật.
+                    response.Code = "2";
+                    response.Message = $"Something wrong happened with Database, please Check the configuration: {sqlex.GetType()} - {sqlex.Message}";
+                    return response;
                 }
                 catch (DbUpdateConcurrencyException ex)
                 {
                     await connection.RollbackAsync();
-                    return null;
+                    //lỗi xảy ra khi có sự xung đột giữa các bản ghi trong cơ sở dữ liệu khi cố gắng cập nhật.
+                    response.Code = "3";
+                    response.Message = $"Concurrency error or Conflict happened : {ex.GetType()} - {ex.Message}";
+                    return response;
                 }
                 catch (DbUpdateException ex)
                 {
                     await connection.RollbackAsync();
-                    return null;
+                    //lỗi xảy ra khi không thể cập nhật cơ sở dữ liệu, có thể do các vấn đề về dữ liệu hoặc các ràng buộc.
+                    response.Code = "4";
+                    response.Message = $"Database update error: {ex.GetType()} - {ex.Message}";
+                    return response;
                 }
                 catch (OperationCanceledException ex)
                 {
                     await connection.RollbackAsync();
-                    return null;
+                    //Lỗi xuất hiện do timeout hoặc yêu cầu dừng quá trình.
+                    response.Code = "5";
+                    response.Message = $"Operation canceled: {ex.GetType()} - {ex.Message}";
+                    return response;
                 }
-
+                catch (Exception ex)
+                {
+                    await connection.RollbackAsync();
+                    response.Code = "6";
+                    response.Message = $"An unexpected error occurred: {ex.GetType()} - {ex.Message}";
+                    return response;
+                }
             }
 
         }
 
-
-
-        public async Task<TransactionType> Update(TransactionType entity)
+        public async Task<ResultService<TransactionType>> Update(TransactionType entity)
         {
-            try
+            var response = new ResultService<TransactionType>();
+
+            var getEntityID = await this.Get(entity.ID);
+            if (getEntityID.Code == "-1")
             {
-                using (var connection = _Context.Database.BeginTransaction())
+                return new ResultService<TransactionType>()
                 {
-                    var TransType = await Get(entity.ID);
-                    var newTransType = await _Context.TransactionTypes.FindAsync(TransType.RowPointer);
-                    if (newTransType == null)
-                        return null;
+                    Code = getEntityID.Code,
+                    Message = "Entity not found"
+                };
 
+            }
+            var newEntity = await _Context.TransactionTypes.FindAsync(getEntityID.Data.RowPointer);
+            if (newEntity == null)
+            {
+                return new ResultService<TransactionType>()
+                {
+                    Code = "-1",
+                    Message = "Entity not found"
+                };
 
-                    newTransType.Description = entity.Description;
-                    newTransType.DocumentTypeID = entity.DocumentTypeID;
-                    newTransType.TransactionTypeCode = entity.TransactionTypeCode;
-                    newTransType.TransactionTypeName = entity.TransactionTypeName;
-                    newTransType.UpdatedBy = entity.UpdatedBy;
-                    newTransType.UpdatedDate = DateTime.Now;
-
-
+            }
+            using (var connection = await _Context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    newEntity.Description = entity.Description;
+                    newEntity.UpdatedDate = DateTime.UtcNow;
+                    newEntity.UpdatedBy = entity.UpdatedBy;
+                    newEntity.DocumentTypeID = entity.DocumentTypeID;
+                    newEntity.TransactionTypeCode = entity.TransactionTypeCode;
+                    newEntity.TransactionTypeName = entity.TransactionTypeName;
 
                     await _Context.SaveChangesAsync();
                     await connection.CommitAsync();
-                    return newTransType;
-
-
-
+                    response.Code = "0";
+                    response.Data = newEntity;
+                    response.Message = "Update entity Successfully";
+                    return response;
                 }
-            }
-            catch (InvalidOperationException ex)
-            {
-                return null;
-            }
-            catch (ArgumentException ex)
-            {
-                return null;
-            }
-            catch (OperationCanceledException ex)
-            {
-                return null;
-            }
+                catch (SqlException sqlex)
+                {
+                    await connection.RollbackAsync();
+                    response.Code = "2";
+                    response.Message = $"Something wrong happened with Database, please Check the configuration: {sqlex.GetType()} - {sqlex.Message}";
+                    return response;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    await connection.RollbackAsync();
+                    response.Code = "3";
+                    response.Message = $"Concurrency error or Conflict happened : {ex.GetType()} - {ex.Message}";
+                    return response;
+                }
+                catch (DbUpdateException ex)
+                {
+                    await connection.RollbackAsync();
+                    response.Code = "4";
+                    response.Message = $"Database update error: {ex.GetType()} - {ex.Message}";
+                    return response;
+                }
+                catch (OperationCanceledException ex)
+                {
+                    await connection.RollbackAsync();
+                    response.Code = "5";
+                    response.Message = $"Operation canceled: {ex.GetType()} - {ex.Message}";
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    await connection.RollbackAsync();
+                    response.Code = "6";
+                    response.Message = $"An unexpected error occurred: {ex.GetType()} - {ex.Message}";
+                    return response;
+                }
 
+            }
         }
-        public async Task<string> Delete(int id)
+
+
+        async Task<ResultService<string>> ICRUD_Service_V2<TransactionType, int>.Delete(int id)
         {
+            ResultService<string> resultService = new ResultService<string>();
             try
             {
                 var entity = await this.Get(id);
-                if (entity == null)
+                if (entity.Code == "-1")
                 {
-                    throw new Exception("Entity not found!");
+                    return new ResultService<string>()
+                    {
+                        Code = entity.Code,
+                        Data = null,
+                        Message = "Entity not found"
+                    };
                 }
 
-                _Context.TransactionTypes.Remove(entity);
+                _Context.TransactionTypes.Remove(entity.Data);
                 await _Context.SaveChangesAsync();
-                return "Entity deleted successfully";
+                resultService.Code = "0";
+                resultService.Message = "Entity deleted successfully";
+                return resultService;
             }
             catch (DbUpdateConcurrencyException ex)
             {
-                return $"{ex.GetType()}, {ex.Message}";
+                return new ResultService<string>()
+                {
+                    Code = "2",
+                    Data = null,
+                    Message = $"{ex.GetType()}, {ex.Message}"
+                };
+
             }
             catch (DbUpdateException ex)
             {
-                return $"{ex.GetType()}, {ex.Message}";
+                return new ResultService<string>()
+                {
+                    Code = "3",
+                    Data = null,
+                    Message = $"{ex.GetType()}, {ex.Message}"
+                };
             }
             catch (OperationCanceledException ex)
             {
-                return $"{ex.GetType()}, {ex.Message}";
+                return new ResultService<string>()
+                {
+                    Code = "4",
+                    Data = null,
+                    Message = $"{ex.GetType()}, {ex.Message}"
+                };
+            }
+            catch (SqlException ex)
+            {
+                return new ResultService<string>()
+                {
+                    Code = "5",
+                    Data = null,
+                    Message = $"{ex.GetType()}, {ex.Message}"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResultService<string>()
+                {
+                    Code = "6",
+                    Data = null,
+                    Message = $"{ex.GetType()}, {ex.Message}"
+                };
             }
         }
+
+
     }
 }
--- Web_Service/API_Application/Controllers/MasterData/TransactionTypeController.cs
+++ Web_Service/API_Application/Controllers/MasterData/TransactionTypeController.cs
@@ -11,12 +11,12 @@ namespace API_Application.Controllers.MasterData
     public class TransactionTypeController : ControllerBase
     {
         private readonly ITransactionTypeProvider _transactionTypeProvider;
-        private readonly ICRUD_Service<TransactionType, int> _ICRUD_Service;
+        private readonly ICRUD_Service_V2<TransactionType, int> _ICRUD_Service_V2;
 
-        public TransactionTypeController(ITransactionTypeProvider transactionTypeProvider, ICRUD_Service<TransactionType, int> iCRUD_Service)
+        public TransactionTypeController(ITransactionTypeProvider transactionTypeProvider, ICRUD_Service_V2<TransactionType, int> iCRUD_Service)
         {
             _transactionTypeProvider = transactionTypeProvider;
-            _ICRUD_Service = iCRUD_Service;
+            _ICRUD_Service_V2 = iCRUD_Service;
         }
 
         [HttpGet]
@@ -24,8 +24,8 @@ namespace API_Application.Controllers.MasterData
         [Produces("application/json")]
         public async Task<IActionResult> GetAll()
         {
-            var rs = await _ICRUD_Service.GetAll();
-            return rs == null ? BadRequest() : Ok(rs);
+            var rs = await _ICRUD_Service_V2.GetAll();
+            return rs.Code == "0" ? Ok(rs.Data) : BadRequest(rs.Message);
 
         }
 
@@ -34,8 +34,8 @@ namespace API_Application.Controllers.MasterData
         [Produces("application/json")]
         public async Task<IActionResult> GetByID([FromQuery] int ID)
         {
-            var rs = await _ICRUD_Service.Get(ID);
-            return rs == null ? BadRequest() : Ok(rs);
+            var rs = await _ICRUD_Service_V2.Get(ID);
+            return rs.Code == "0" ? Ok(rs.Data) : BadRequest(rs.Message);
 
         }
 
@@ -46,8 +46,8 @@ namespace API_Application.Controllers.MasterData
 
         public async Task<IActionResult> Save([FromBody] TransactionType TransactionType)
         {
-            var rs = await _ICRUD_Service.Create(TransactionType);
-            return rs == null ? BadRequest() : Ok(rs);
+            var rs = await _ICRUD_Service_V2.Create(TransactionType);
+            return rs.Code == "0" ? Ok(rs.Data) : BadRequest(rs.Message);
         }
 
         [HttpPut]
@@ -56,8 +56,8 @@ namespace API_Application.Controllers.MasterData
 
         public async Task<IActionResult> Update([FromBody] TransactionType TransactionType)
         {
-            var rs = await _ICRUD_Service.Update(TransactionType);
-            return rs == null ? BadRequest() : Ok(rs);
+            var rs = await _ICRUD_Service_V2.Update(TransactionType);
+            return rs.Code == "0" ? Ok(rs.Data) : BadRequest(rs.Message);
 
         }
         [HttpDelete]
@@ -66,11 +66,9 @@ namespace API_Application.Controllers.MasterData
 
         public async Task<IActionResult> Delete(int id)
         {
-            var rs = await _ICRUD_Service.Delete(id);
-            return rs == null ? BadRequest() : Ok(rs);
-
+            var rs = await _ICRUD_Service_V2.Delete(id);
+            return rs.Code == "0" ? Ok(rs.Message) : BadRequest(rs.Message);
         }
 
-        
     }
 }
