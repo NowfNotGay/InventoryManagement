8ef98a9fde9ca96ca6898aa93d8c77eb2b672361
Er2g0n <hnguyenngoc.h@gmail.com>
2025-03-06 18:14:45 +0700

    hai - push code

--- Web_Service/API_Application/Controllers/ProductClassification/BrandController.cs
+++ Web_Service/API_Application/Controllers/ProductClassification/BrandController.cs
@@ -4,6 +4,7 @@ using Base.ProductProperties;
 using Core.MasterData;
 using Core.ProductClassification;
 using Core.ProductProperties;
+using Core.WarehouseManagement;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,21 +13,22 @@ namespace API_Application.Controllers.ProductClassification;
 [ApiController]
 public class BrandController : ControllerBase
 {
-    private readonly ICRUD_Service<Brand, int> _brandService;
+    private readonly ICRUD_Service<Brand, int> _ICRUD_Service;
     private readonly IBrandProvider _brandProvider;
 
-    public BrandController(ICRUD_Service<Brand, int> brandService, IBrandProvider brandProvider)
+    public BrandController(ICRUD_Service<Brand, int> iCRUD_Service, IBrandProvider brandProvider)
     {
-        _brandService = brandService;
+        _ICRUD_Service = iCRUD_Service;
         _brandProvider = brandProvider;
     }
+    #region Normal CRUD
 
     [HttpGet]
     [Consumes("application/json")]
     [Produces("application/json")]
     public async Task<IActionResult> GetAll()
     {
-        var rs = await _brandService.GetAll();
+        var rs = await _ICRUD_Service.GetAll();
         return rs == null ? BadRequest("No brands found") : Ok(rs);
     }
 
@@ -35,7 +37,7 @@ public class BrandController : ControllerBase
     [Produces("application/json")]
     public async Task<IActionResult> GetById(int id)
     {
-        var rs = await _brandService.Get(id);
+        var rs = await _ICRUD_Service.Get(id);
         return rs == null ? NotFound($"Brand with ID {id} not found") : Ok(rs);
     }
 
@@ -44,7 +46,7 @@ public class BrandController : ControllerBase
     [Produces("application/json")]
     public async Task<IActionResult> Create([FromBody] Brand brand)
     {
-        var rs = await _brandService.Create(brand);
+        var rs = await _ICRUD_Service.Create(brand);
         return rs == null ? BadRequest("Failed to create brand") : Ok(rs);
     }
 
@@ -53,7 +55,7 @@ public class BrandController : ControllerBase
     [Produces("application/json")]
     public async Task<IActionResult> Update([FromBody] Brand brand)
     {
-        var rs = await _brandService.Update(brand);
+        var rs = await _ICRUD_Service.Update(brand);
         return rs == null ? BadRequest("Failed to update brand") : Ok(rs);
     }
 
@@ -62,7 +64,32 @@ public class BrandController : ControllerBase
     [Produces("application/json")]
     public async Task<IActionResult> Delete(int id)
     {
-        var rs = await _brandService.Delete(id);
+        var rs = await _ICRUD_Service.Delete(id);
         return rs == null ? BadRequest("Failed to delete brand") : Ok(rs);
     }
+    #endregion
+
+    #region Dapper CRUD
+
+    [HttpPost("SaveByDapper")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+
+    public async Task<IActionResult> SaveByDapper([FromBody] Brand brand)
+    {
+        var rs = await _brandProvider.SaveByDapper(brand);
+        return rs.Code == "0" ? Ok(rs.Data) : BadRequest(rs.Message);
+    }
+    [HttpDelete("DeleteByDapper")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+
+    public async Task<IActionResult> DeleteByDapper(string brandCode)
+    {
+        var rs = await _brandProvider.DeleteByDapper(brandCode);
+        return rs.Code == "0" ? Ok(rs.Message) : BadRequest(rs.Message);
+    }
+
+
+    #endregion
 }
