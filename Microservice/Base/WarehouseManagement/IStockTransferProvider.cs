bd97a8b6f410a07715f7da49257a281ba8160682
Hoangduy2982003 <125873702+Hoangduy2982003@users.noreply.github.com>
2025-03-28 12:22:47 +0700

    Duy commit - sửa sửa 1 xíu

--- Microservice/Base/WarehouseManagement/IStockTransferProvider.cs
+++ Microservice/Base/WarehouseManagement/IStockTransferProvider.cs
@@ -11,6 +11,7 @@ namespace Base.WarehouseManagement
         Task<ResultService<string>> DeleteDetail(List<StockTransferDetail> entity);
         Task<ResultService<IEnumerable<StockTransferDetail>>> GetDetailByStockTransferID(string stCode);
         Task<ResultService<string>> Delete_HeaderAndDetail(int stID);
+        Task<ResultService<StockTransfer_Param>> Save(StockTransfer_Param entity);
 
     }
 }
--- Microservice/Servicer/WarehouseManagement/StockTransferProvider.cs
+++ Microservice/Servicer/WarehouseManagement/StockTransferProvider.cs
@@ -767,5 +767,87 @@ namespace Servicer.WarehouseManagement
                 return response;
             }
         }
+
+        public async Task<ResultService<StockTransfer_Param>> Save(StockTransfer_Param entity)
+        {
+            var response = new ResultService<StockTransfer_Param>();
+            if (entity == null)
+            {
+                return new ResultService<StockTransfer_Param>()
+                {
+                    Code = "1",
+                    Message = "Entity is not valid"
+                };
+            }
+            try
+            {
+                string Message = string.Empty;
+
+                string conn = General.DecryptString(_configuration.GetConnectionString(_moduleDapper));
+                using (var connection = new SqlConnection(conn))
+                {
+                    await connection.OpenAsync();
+                    var param = new DynamicParameters();
+                    param.Add("@CreatedBy", entity.CreatedBy);
+                    param.Add("@udtt_Header", General.ConvertToDataTable(entity.StockTransfers).AsTableValuedParameter("UDTT_StockTransferHeader"));
+                    param.Add("@udtt_Detail", General.ConvertToDataTable(entity.StockTransferDetails).AsTableValuedParameter("UDTT_StockTransferDetail"));
+                    param.Add("@Message", Message, dbType: DbType.String, direction: ParameterDirection.Output, size: 500);
+                    await connection.QueryAsync<GoodsReceiptNote>("StockTransfer_Create",
+                       param,
+                       commandType: CommandType.StoredProcedure,
+                          commandTimeout: TimeoutInSeconds);
+                    var resultMessage = param.Get<string>("@Message");
+
+                    if (resultMessage.Contains("OK"))
+                    {
+                        response.Code = "0"; // Success
+                        response.Message = "Save Successfully";
+                    }
+                    else
+                    {
+                        response.Code = "-999"; // Success
+                        response.Message = "Failed";
+                    }
+
+                    return response;
+
+                }
+            }
+            catch (SqlException sqlex)
+            {
+
+                response.Code = "2";
+                response.Message = $"Something wrong happened with Database, please Check the configuration: {sqlex.GetType()} - {sqlex.Message}";
+                return response;
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+
+                response.Code = "3";
+                response.Message = $"Concurrency error or Conflict happened : {ex.GetType()} - {ex.Message}";
+                return response;
+            }
+            catch (DbUpdateException ex)
+            {
+
+                response.Code = "4";
+                response.Message = $"Database update error: {ex.GetType()} - {ex.Message}";
+                return response;
+            }
+            catch (OperationCanceledException ex)
+            {
+
+                response.Code = "5";
+                response.Message = $"Operation canceled: {ex.GetType()} - {ex.Message}";
+                return response;
+            }
+            catch (Exception ex)
+            {
+
+                response.Code = "6";
+                response.Message = $"An unexpected error occurred: {ex.GetType()} - {ex.Message}";
+                return response;
+            }
+        }
     }
 }
