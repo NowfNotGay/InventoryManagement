565919a55d7cd7e0995c092d6f9231b6886dde04
Eragon <hnguyenngoc.h@gmail.com>
2025-06-28 01:31:00 +0700

    chore: refactor IWarehouseProvider

---
+++ Microservice/Base/WarehouseManagement/IWarehouseProvider.cs
@@ -0,0 +1,16 @@
+﻿using Core.BaseClass;
+using Core.WarehouseManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.WarehouseManagement;
+public interface IWarehouseProvider
+{
+    Task<ResultService<Warehouse>> SaveByDapper(Warehouse entity);
+    Task<ResultService<Warehouse>> GetByCode(string warehouseCode);
+    Task<ResultService<string>> DeleteByDapper(string warehouseCode);
+
+}
