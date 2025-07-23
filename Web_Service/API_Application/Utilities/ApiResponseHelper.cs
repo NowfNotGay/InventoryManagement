ce6b8d8877a1575f3497879832d707bff5e8554d
Eragon <hnguyenngoc.h@gmail.com>
2025-07-23 14:01:45 +0700

    feat: add file ApiResponseHelper

---
+++ Web_Service/API_Application/Utilities/ApiResponseHelper.cs
@@ -0,0 +1,17 @@
+﻿using Core.BaseClass;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Application.Utilities;
+
+public static class ApiResponseHelper
+{
+    public static IActionResult HandleResult<T>(ResultService<T> result, ControllerBase controller)
+    {
+        return result.Code switch
+        {
+            "0" => controller.Ok(result),
+            "1" => controller.NotFound(result),
+            _ => controller.BadRequest(result)
+        };
+    }
+}
